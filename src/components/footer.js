e14195967a9277b6305821af1300d3dfb591d4f4
Vaishali785 <aggarwal.vaishali785@gmail.com>
2022-09-10 12:40:52 +0530

    dynamic audio changing

--- src/components/footer.js
+++ src/components/footer.js
@@ -14,13 +14,14 @@ import SurroundSoundIcon from '@mui/icons-material/SurroundSound';
 import { useRef } from "react";
 import { useDataLayerValue } from "./dataLayer";
 import './footer.css';
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import dhokha from '../assets/dhokha.mp3';
 
 export default function Footer() {
     const [{currentSong},dispatch]=useDataLayerValue();
     const [isPlaying,setPlaying]=useState(false);
     const [song,setSong]=useState(dhokha);
+    const audioRef=useRef();
     // const [value,setValue]=useState(30);
     // const audioPlayer=useRef();
     const changePlayPause=()=>{
@@ -28,29 +29,35 @@ export default function Footer() {
       setPlaying(!isPlaying);
       // audioPlayer.current.play();
     }
-    // const handleChange=(e)=>{
-    //   console.log(e)
-    //   setValue(e)
-    // }
-    
+    const updateSong=(source)=>{
+      setSong(source);
+      if(audioRef.current){
+        audioRef.current.pause();
+        audioRef.current.load();
+        audioRef.current.play();
+      }
+    }
+    useEffect(()=> {
+      updateSong(currentSong.preview_url);
+    },[currentSong]);
     console.log("xxxxxx",song);
-    console.log("yyyy",currentSong.preview_url);
+    console.log("yyyy",currentSong?.album?.artists[0].name );
     
     return (
       <div className="footer">
 
         <div className="left">
-          <img className="song-pic" src={currentSong && currentSong.length>1 ? currentSong?.album?. images[2].url : 'https://i.scdn.co/image/ab67616d00004851598b54ac053ca26cb7ec7c38'} />
+          <img className="song-pic" src={currentSong?.album?. images[2].url || 'https://i.scdn.co/image/ab67616d00004851598b54ac053ca26cb7ec7c38'} />
           <div className="song-container">
-            <h3 className='song-name'>{currentSong && currentSong.length>1 ? currentSong?.name : "Challa" }</h3>
-            <h4 className="song-other-info">{currentSong && currentSong.length>1 ? currentSong?.name : "Vaishali"}</h4>
+            <h3 className='song-name'>{currentSong?.name || "Challa" }</h3>
+            <h4 className="song-other-info">{ currentSong?.album?.artists[0].name || "Vaishali"}</h4>
           </div>
         </div>
         <div className="center">
-        <audio controls id='audio-player' className='audio-player'>
-            <source src={ currentSong.preview_url || dhokha} type="audio/mpeg" />
+        <audio controls id='audio-player' className='audio-player' ref={audioRef} >
+            <source src={song} />
             Your browser does not support the audio element.
-          </audio>
+        </audio>
         </div>
         <div className="right">
           
